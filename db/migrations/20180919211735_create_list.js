d1f31fa0e469e854cd84dc401c81edeab123973b
Max <MaxHalleran@gmail.com>
2018-09-19 22:39:56 +0000

    added a yin yang symbol

--- db/migrations/20180919211735_create_list.js
+++ db/migrations/20180919211735_create_list.js
@@ -2,6 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('list', function (table) {
     table.increments('id');
     table.integer('user_id');
+    table.string('name');
     table.string('category');
 
     table.foreign('user_id').references('id').inTable('user');
