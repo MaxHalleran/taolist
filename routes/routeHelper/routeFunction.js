425165ea8de6fe48c1bff4f306ac00f5a5dad2b0
Max <MaxHalleran@gmail.com>
2018-09-21 21:51:57 +0000

    paired programming

--- routes/routeHelper/routeFunction.js
+++ routes/routeHelper/routeFunction.js
@@ -1,7 +1,9 @@
 const bcrypt = require('bcrypt');
 
-exports.validateLogin = function(targetPassword, currentPassword) {
-  if (/*bcrypt.compareSync*/(currentPassword === targetPassword)) {
+exports.validateLogin = function validateLogin(targetPassword, currentPassword) {
+  console.log('validateLogin is firing');
+  console.log(targetPassword, currentPassword);
+  if (bcrypt.compareSync(currentPassword === targetPassword)) {
     return true;
   } else {
     return false;
