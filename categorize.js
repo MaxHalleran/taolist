5a4d98b81bee3a8f910a10e398958473da350595
Alex Buck <buckatech@gmail.com>
2018-09-23 22:53:46 -0600

    added giant if/else tree

--- categorize.js
+++ categorize.js
@@ -1,12 +1,11 @@
 const request = require('request');
 const cheerio = require('cheerio');
-const removeNewline = require('newline-remove');
 const language = require('@google-cloud/language');
 const client = new language.LanguageServiceClient();
-let text;
-// Prepares a document, representing the provided text
-let userinput = 'Peanut butter'
 
+// Prepares a document, representing the provided text
+let userinput = 'Harry Potter'
+module.exports = function categorizeStuff(userinput){
 request(`https://en.wikipedia.org/wiki/${userinput}`, function (error, response, html) {
 if (!error && response.statusCode == 200) {
     const $ = cheerio.load(html);
@@ -25,7 +24,62 @@ if (!error && response.statusCode == 200) {
   .then(results => {
     const classification = results[0];
     classification.categories.forEach(category => {  
-        console.log((`|| Name: ${category.name}, Confidence: ${category.confidence}`))
+        output = (`|| Name: ${category.name}, Confidence: ${category.confidence}`)
+        if(output.includes('/Food & Drink')){
+          return ('Food')
+        } else if (output.includes("/Arts & Entertainment")){
+          return ('Watch')
+        } else if (output.includes("/Autos & Vehicles")){
+          return ('Buy')
+        } else if (output.includes("/Books & Literature")){
+          return('Read')
+        } else if (output.includes("/Beauty & Fitness")){
+          return ('Buy')
+        } else if (output.includes("/Business & Industrial")){
+          return ("Other")
+        } else if (output.includes("/Computers & Electronics")){
+          return ('Buy')
+        } else if (output.includes("/Finance")){
+          return ('Other')
+        } else if (output.includes("/Games")){
+          return ("Buy")
+        } else if (output.includes("/Health")){
+          return ("Other")
+        } else if (output.includes("/Hobbies & Leisure")){
+          return ("Other")
+        } else if (output.includes("/Home & Garden")){
+          return ("Buy")
+        } else if (output.includes("/Internet & Telecom")){
+          return ("Other")
+        } else if (output.includes("/Jobs & Education")){
+          return ("Other")
+        } else if (output.includes("/Law & Government")){
+          return ("Other")
+        } else if (output.includes("/News")){
+          return ("Watch")
+        } else if (output.includes("/Online Communities")){
+          return ("Other")
+        } else if (output.includes("/People & Society")){
+          return ("Other")
+        } else if (output.includes("/Pets & Animals")){
+          return ("Buy")
+        } else if (output.includes("/Real Estate")){
+          return ("Other")
+        } else if (output.includes("/Reference")){
+          return ("Read")
+        } else if (output.includes("/Science")){
+          return ("Other")
+        } else if (output.includes("/Sensitive Subjects")){
+          return ("Other")
+        } else if (output.includes("/Shopping")){
+          return ("Buy")
+        } else if (output.includes("/Sports")){
+          return ("Other")
+        } else if (output.includes("/Travel")){
+          return ("Other")
+        } else {
+          return ("Naughty Naughty")
+        }
     });
   })
   .catch(err => {
@@ -33,3 +87,4 @@ if (!error && response.statusCode == 200) {
   });
 };
 });
+}
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sass = require('node-sass-middleware');
 const app = express();
-
 const knexConfig = require('./knexfile');
 const knex = require('knex')(knexConfig[ENV]);
 const morgan = require('morgan');
@@ -22,7 +21,7 @@ const userRoute = require('./routes/user');
 const itemRoute = require('./routes/item');
 const listRoute = require('./routes/list');
 const registerRoute = require('./routes/register');
-
+const categorize = require('./categorize')
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 // The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -30,7 +29,8 @@ app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
-
+let userstuff = "Harry Potter"
+categorize(userstuff)
 app.use(cookieSession({
   name: 'session',
   keys: ['suppalightohousegottariot'],
